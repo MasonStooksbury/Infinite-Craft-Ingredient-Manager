f4a59f9b1496e8365f22722ca078096ef60e7db6
MasonStooksbury <masonstooksbury@gmail.com>
2024-03-03 18:44:46 -0600

    Reorganizing

---
+++ infinite-craft-ingredient-manager/background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onMessage.addListener((message) => {
+    // Take the data from local storage and download it
+    if (message.type === 'export') {
+        // Use the data from the page context to create a Blob and initiate a download
+        const blob = new Blob([message.data], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        chrome.downloads.download({
+            url: url,
+            filename: 'infinite-craft-data.json'
+        }).catch((error) => {
+            console.error('Download failed:', error);
+        });
+    }
+
+    // Reload page (after import)
+    if (message.action === "reloadTab") {
+        chrome.tabs.reload(sender.tab.id);
+    }
+});
